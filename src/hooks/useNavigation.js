6c59a7e582eed2c506952c03d4193a31f54faa8e
Abel Perez <abelper54@gmail.com>
2022-05-15 11:01:41 -0400

    block same origin push

--- src/hooks/useNavigation.js
+++ src/hooks/useNavigation.js
@@ -2,16 +2,29 @@ import { trigger } from "../helpers/events";
 
 // Uses current window location and adds enhanced functionality
 export default function useNavigation() {
+
+  const getNewLocation = (path) => {
+    const {pathname,search,origin} = window.location;
+    const currentPath = pathname + search;
+    const newPath = (path.startsWith("/") ? "" : "/") + path;
+
+    if (currentPath === newPath) return null;
+    return origin + newPath;
+  }
  
   //Function to navigate to given url relative to baseUrl
   const load = (path) => {
-    const newLocation = window.location.origin + (path.startsWith("/") ? "" : "/") + path;
+    const newLocation = getNewLocation(path);
+
+    if (!newLocation) return;
     window.location.assign(newLocation);
   };
 
   //Function to handle client-side transitions
   const push = (path, state = null) => {
-    const newLocation = window.location.origin + (path.startsWith("/") ? "" : "/") + path;
+    const newLocation = getNewLocation(path);
+
+    if (!newLocation) return;
     window.history.pushState(state, "", newLocation);
     trigger("locationpush");
   };
